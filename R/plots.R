5fc6214d66cb873726f2f3a5c027627ff670d69d
Sebastian Sauer <ssauer@posteo.de>
2023-05-24 23:05:26 +0200

    ms update

--- R/plots.R
+++ R/plots.R
@@ -1,8 +1,9 @@
 plot_preds_summarized <- function(preds_summarized) {
   
-  out <- preds_summarized
+  #out <- preds_summarized
   
-  out %>% 
+  out <- 
+    preds_summarized %>% 
     ggplot(aes(x = account_name, y = prop, fill = .pred_class, position = "fill")) +
     geom_col() +
     facet_wrap(~ year)
